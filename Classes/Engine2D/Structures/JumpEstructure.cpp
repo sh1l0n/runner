4514adf81df16a3d59bb4785e895c68c861a09ca
pramon <prm5@alu.ua.es>
2017-03-03 12:50:56 +0100

    Jump est OK

--- Classes/Engine2D/Structures/JumpEstructure.cpp
+++ Classes/Engine2D/Structures/JumpEstructure.cpp
@@ -2,4 +2,15 @@
 // Created by Pablo Ramon on 3/3/17.
 //
 
-#include "JumpEstructure.h"
+#include "JumpEstructure.hpp"
+/**
+* @brief Creates a jump structure with default values
+*/
+Structures::
+JumpEstructure::JumpEstructure() : Structures::BaseStructure()
+{
+    this->_height =K_DEFAULT_HEIGHT-1;
+    this->_width = K_DEFAULT_WIDTH-1;
+    this->_data.resize(this->_height);
+
+}
--- Classes/Engine2D/Structures/JumpEstructure.hpp
+++ Classes/Engine2D/Structures/JumpEstructure.hpp
@@ -5,10 +5,39 @@
 #ifndef MYGAME_JUMPESTRUCTURE_H
 #define MYGAME_JUMPESTRUCTURE_H
 
+#pragma once
 
-class JumpEstructure {
+#include "BaseStructure.hpp"
 
-};
+namespace Structures {
+    /**
+	* @short Class JumpEstructure, create an structure with damage floor and some platforms to jump along it
+	*/
+    class JumpEstructure: public BaseStructure {
+    private:
+        /** @short Default height of the structure */
+        const static unsigned char K_DEFAULT_HEIGHT = 0x03;
 
+        /** @short Default width of the structure */
+        const static unsigned char K_DEFAULT_WIDTH = 0x020;
+
+        /** @short Default max value of x jump of the block structure */
+        const static unsigned char K_DEFAULT_MAX_X_JUMP = 0x01;
+
+        /** @short Default max value of y jump of the block structure */
+        const static unsigned char K_DEFAULT_MAX_Y_JUMP = 0x01;
+
+        unsigned int  _maxXJump;
+        unsigned int  _maxYJump;
+
+
+
+
+    public:
+        /** @short Creates a new jump structure with default values */
+        JumpEstructure();
+
+    };
+}
 
 #endif //MYGAME_JUMPESTRUCTURE_H
